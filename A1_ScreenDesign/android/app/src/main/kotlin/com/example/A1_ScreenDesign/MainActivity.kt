8446ddac69256955ab6f64998a2abfbac175e50e
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2020-10-22 18:01:20 +0200

    Project folder added

---
+++ A1_ScreenDesign/android/app/src/main/kotlin/com/example/A1_ScreenDesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.A1_ScreenDesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ A1_ScreenDesign/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Screen Design',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _MainScreen(),
+    );
+  }
+}
+
+class _MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Center(
+              child: Text(
+            'Amazing Text',
+            style: TextStyle(
+              color: Colors.lightGreen,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+        ],
+      ),
+    );
+  }
+}
